1f3bc87380b43a11c723409a47272c62f2588380
Simeon Delchev <sidelch9@gmail.com>
2021-10-06 17:44:33 +0300

    renamed the class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContainerComponentComponent } from './container-component/container-component.component';
+import { ContainerComponent } from './container-component/container-component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponentComponent
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
