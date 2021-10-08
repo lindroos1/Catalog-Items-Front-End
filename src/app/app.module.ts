04754a2ecde2ae2c7be5525eac0bbc4fc2865c7d
Simeon Delchev <sidelch9@gmail.com>
2021-10-08 16:39:14 +0300

    working routhing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,28 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './container-component/container-component';
 import { ItemComponent } from './item/item.component';
 import { RequestFormComponent } from './request-form/request-form.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     ItemComponent,
-    RequestFormComponent
+    RequestFormComponent, 
+   
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    RouterModule.forRoot([
+     { path: '', redirectTo: '/main', pathMatch: 'full' },
+
+      {path: 'request', component: RequestFormComponent},
+     {path: 'main ', component: ItemComponent},
+      {path: 'main', component: ContainerComponent}
+     ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
